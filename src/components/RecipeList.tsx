17d5f1d98825a1bf3690048e2fb78e4b886fef0c
Fabiiisb <fabiiisb@hotmail.com>
2024-09-17 19:12:06 -0300

    feat(RecipeList): better css

--- src/components/RecipeList.tsx
+++ src/components/RecipeList.tsx
@@ -23,25 +23,25 @@ interface CardListProps {
 
 const CardList = ({ recipes }: CardListProps) => {
   return (
-    <div className="mx-auto p-4">
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 gap-4 auto-rows-[minmax(200px,auto)]">
+    <div className="mx-auto">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 gap-4 auto-rows-[minmax(200px,auto)] ">
         {recipes.map((recipe) => (
           <Link
             href={`/receta/${recipe.id}`}
             key={recipe.id}
           >
             <Card
-              className="bg-white overflow-hidden rounded-md border border-forest-100 "
+              className="bg-white/65 overflow-hidden rounded-2xl "
               title={recipe.title}
             >
               <CardHeader className="p-0 relative group">
                 <Image
-                  className="w-full max-h-[200px] object-cover rounded-md "
+                  className="w-full max-h-[200px] object-cover rounded-2xl "
                   src={testImg}
                   alt="Imagen receta"
                 />
                 <div
-                  className="scroll-container backdrop-blur-sm bg-gradient-to-r from-black/70 absolute p-2 !m-0 w-full h-full opacity-0 group-hover:opacity-100 rounded-md overflow-auto text-white transition-opacity duration-300"
+                  className="scroll-container backdrop-blur-sm bg-gradient-to-r from-black/70 absolute p-2 pl-4 !m-0 w-full h-full opacity-0 group-hover:opacity-100 rounded-2xl overflow-auto text-white transition-opacity duration-300"
                 >
                   <p className="mb-1">Ingredientes:</p>
                   <ul className="list-disc list-outside pl-5 space-y-1 marker:text-[#ed5c4c]">
@@ -54,7 +54,7 @@ const CardList = ({ recipes }: CardListProps) => {
                 </div>
               </CardHeader>
               <CardFooter
-                className="flex flex-col gap-3"
+                className="flex flex-col gap-3 px-2"
               >
                 <CardTitle className="text-forest-900 tracking-wide h-5	truncate">
                   {recipe.title}
