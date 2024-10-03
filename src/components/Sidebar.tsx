33da95fec06d53a8760c810d7298139f0927bf5e
Fabiiisb <fabiiisb@hotmail.com>
2024-10-03 11:26:13 -0300

    feat(sidebar): open sidebar opacity, button saved recipes

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import Link from 'next/link'
 import { Button } from "@/components/ui/button"
 import { ScrollArea } from "@/components/ui/scroll-area"
-import { HomeIcon, InboxIcon, UsersIcon, SettingsIcon, MenuIcon, XIcon } from 'lucide-react'
+import { HomeIcon, InboxIcon, UsersIcon, SettingsIcon, MenuIcon, XIcon, Microwave } from 'lucide-react'
 
 type SidebarProps = {
   children: React.ReactNode;
@@ -17,14 +17,23 @@ export default function Sidebar({ children }: SidebarProps) {
     { name: 'Bandeja de entrada', icon: InboxIcon, href: '/inbox' },
     { name: 'Usuarios', icon: UsersIcon, href: '/users' },
     { name: 'Configuración', icon: SettingsIcon, href: '/settings' },
+    { name: 'Recetas guardadas', icon: Microwave, href: '/recetasguardadas' },
   ]
 
   return (
     <>
+      {sidebarOpen && (
+        <div
+          className="fixed inset-0 bg-black bg-opacity-50 z-20 lg:hidden"
+          onClick={() => setSidebarOpen(false)}
+        />
+      )}
       <aside className={`bg-[#fff] border-r w-64 absolute inset-y-0 left-0 transform ${sidebarOpen ? 'translate-x-0' : '-translate-x-full'} lg:relative lg:translate-x-0 transition duration-200 ease-in-out z-30`}>
         <div className="h-full flex flex-col">
-          <div className="p-4 border-b flex justify-between items-center">
-            <h2 className="text-lg font-semibold">FlavorTales</h2>
+          <div className="px-4 border-b flex justify-between items-center py-1 h-[49px]">
+            <h2 className="text-lg font-semibold">
+              {process.env.NEXT_PUBLIC_PAGE_NAME}
+            </h2>
             <Button
               variant="ghost"
               size="icon"
@@ -52,16 +61,35 @@ export default function Sidebar({ children }: SidebarProps) {
           </ScrollArea>
         </div>
       </aside>
+
       <div className="flex-1 flex flex-col overflow-hidden">
         <header className="bg-background border-b flex items-center lg:hidden px-1 py-1">
           <Button
+            className='text-muted-foreground hover:text-black'
             variant={'ghost'}
             size="icon"
             onClick={() => setSidebarOpen(true)}
           >
             <MenuIcon className="h-4 w-4" />
           </Button>
+          <div className='w-full flex'>
+            <Link href={'/'} className='inline-flex mx-auto items-center'>
+              {process.env.NEXT_PUBLIC_PAGE_NAME}
+            </Link>
+            <div className='flex gap-1'>
+              <Link href={navItems[4].href}>
+                <Button
+                  className='text-muted-foreground hover:text-black'
+                  variant={'ghost'}
+                  size="icon"
+                >
+                  <Microwave className="h-4 w-4" />
+                </Button>
+              </Link>
+            </div>
+          </div>
         </header>
+
         <main className='flex-1 overflow-auto'>
           {children}
         </main>
