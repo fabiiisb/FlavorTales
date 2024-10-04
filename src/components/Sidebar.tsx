575443f4e0e111487e6d49a531cccbfa70411a10
Fabiiisb <fabiiisb@hotmail.com>
2024-10-04 12:35:20 -0300

    feat(sidebar): logged component and unlogged component

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,18 +1,36 @@
 'use client'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Link from 'next/link'
 import { Button } from "@/components/ui/button"
 import { ScrollArea } from "@/components/ui/scroll-area"
 import { HomeIcon, InboxIcon, UsersIcon, SettingsIcon, MenuIcon, XIcon, Microwave } from 'lucide-react'
 import Image from 'next/image'
 import logo from '@/img/logo1.png'
+import { useSession, signOut } from 'next-auth/react'
+import { Skeleton } from './ui/skeleton'
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
 
 type SidebarProps = {
   children: React.ReactNode;
 };
 
 export default function Sidebar({ children }: SidebarProps) {
+  const { data: session, status } = useSession()
   const [sidebarOpen, setSidebarOpen] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    if (status === 'loading') {
+      setIsLoading(true)
+    } else {
+      setIsLoading(false)
+    }
+  }, [status])
 
   const navItems = [
     { name: 'Inicio', icon: HomeIcon, href: '/' },
@@ -39,7 +57,7 @@ export default function Sidebar({ children }: SidebarProps) {
               href={'/'}
               className='flex lg:flex-col justify-center items-center'
             >
-              <Image src={logo} alt='logo' className='w-8 h-8 mr-1 lg:w-16 lg:h-16 lg:mr-0' />
+              <Image src={logo} alt='logo' className='w-8 h-8 mr-2 lg:w-16 lg:h-16 lg:mr-0' />
               <h2 className="text-lg font-semibold">
                 {process.env.NEXT_PUBLIC_PAGE_NAME}
               </h2>
@@ -54,24 +72,28 @@ export default function Sidebar({ children }: SidebarProps) {
             </Button>
           </div>
           <div className='flex flex-col gap-2 px-4 py-3 border-b  justify-center items-center'>
-            <Link href={'/login'}>
-              <Button
-                className='w-40'
-              >
-                <UsersIcon className='h-5 w-5 mr-1' />
-                Iniciar sesión
-              </Button>
-            </Link>
+            {
+              isLoading ? (
+                <div className='flex flex-col gap-2'>
+                  <Skeleton className='w-40 h-10 rounded-2xl'>
+                  </Skeleton>
+                  <Skeleton className='w-40 h-10 rounded-2xl'>
+                  </Skeleton>
+                </div>
 
-            <Link href={'/signup'}>
-              <Button
-                className='w-40'
-                variant={'outline'}
-              >
-                <UsersIcon className='h-5 w-5 mr-1' />
-                Unirse
-              </Button>
-            </Link>
+              )
+                :
+                (
+                  session?.user ?
+                    <LoggedButtons
+                      username={session?.user.name}
+                      image={session?.user.image}
+                    />
+                    :
+                    <SessionOffButtons />
+                )
+            }
+
           </div>
           <ScrollArea className="flex-grow">
             <nav className="space-y-2 p-4">
@@ -124,6 +146,74 @@ export default function Sidebar({ children }: SidebarProps) {
           {children}
         </main>
       </div>
+    </>
+  )
+}
+
+const LoggedButtons = ({ username, image }: { username: string | null | undefined, image: string | null | undefined }) => {
+  return (
+    <>
+      <Accordion type="multiple" className="w-full ">
+        <AccordionItem value="item-1" className='!border-b-0 '>
+          <AccordionTrigger className=''>
+            <div className='flex items-center'>
+              <Image
+                src={image ?? 'https://res.cloudinary.com/dpwjyqrss/image/upload/v1718032802/UserPlaceholder.jpg'}
+                alt='Imagen usuario'
+                className='rounded-full mr-2'
+                width={35}
+                height={35}
+              />
+              <p className='truncate max-w-[164px]'>
+                {username}
+              </p>
+            </div>
+          </AccordionTrigger>
+          <AccordionContent className=''>
+            <Link href={'/test'}>
+              <Button
+                className='w-full'
+              >
+                Test
+              </Button>
+            </Link>
+          </AccordionContent>
+          <AccordionContent className=''>
+            <Button
+              onClick={() => signOut()}
+              className='w-full'
+              variant={'destructive'}
+            >
+              Cerrar sesión
+            </Button>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </>
+  )
+}
+
+const SessionOffButtons = () => {
+  return (
+    <>
+      <Link href={'/login'}>
+        <Button
+          className='w-40'
+        >
+          <UsersIcon className='h-5 w-5 mr-1' />
+          Iniciar sesión
+        </Button>
+      </Link>
+
+      <Link href={'/signup'}>
+        <Button
+          className='w-40'
+          variant={'outline'}
+        >
+          <UsersIcon className='h-5 w-5 mr-1' />
+          Unirse
+        </Button>
+      </Link>
     </>
   )
 }
