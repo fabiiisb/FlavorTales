8f177c61672db0d2bdb74fa03651e250ceb61094
Fabiiisb <fabiiisb@hotmail.com>
2024-10-07 13:06:55 -0300

    feat(nextconfig, dbconn): minor changes

--- src/utils/db/dbConn.ts
+++ src/utils/db/dbConn.ts
@@ -1,10 +1,13 @@
 import mongoose from 'mongoose';
 
 export async function getConn() {
-  mongoose.connect(process.env.STRING_CONNECTION as string)
-    .then(() => {
-      console.log('db connected success')
-    }).catch(err => {
-      console.log(err)
-    })
+  if (mongoose.connection.readyState === 0) {
+    await mongoose.connect(process.env.STRING_CONNECTION as string)
+      .then(() => {
+        console.log('Database connected successfully');
+      })
+      .catch(err => {
+        console.log('Database connection failed:', err);
+      });
+  }
 }
